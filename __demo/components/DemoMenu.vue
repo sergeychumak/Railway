c7276314bd45357518cfe0d6453966c96c1cb3e4
sergey <sergeychumakru@gmail.com>
2018-09-30 01:34:53 +0300

    railway-menu

--- __demo/components/DemoMenu.vue
+++ __demo/components/DemoMenu.vue
@@ -1,4 +1,39 @@
 <template>
-  <railway-menu></railway-menu>
+  <railway-menu
+    :items="items"
+  ></railway-menu>
 </template>
 
+
+<script>
+  export default {
+    data: function () {
+      return {
+        items: [
+          { name: 'Элемент 10' },
+          { name: 'Элемент 1' },
+          { name: 'Элемент 1', showChilds: false, childs: [
+              { name: 'Элемент 1.0' },
+              { name: 'Элемент 1.1' },
+              { name: 'Элемент 1.2' }
+            ]
+          },
+          { name: 'Элемент 10' },
+          { name: 'Элемент 1' },
+          { name: 'Элемент 1',  childs: [
+              { name: 'Элемент 1.0' },
+              { name: 'Элемент 1.1' ,  childs: [
+                  { name: 'Элемент 1.0' },
+                  { name: 'Элемент 1.1' },
+                  { name: 'Элемент 1.2' }
+                ] },
+              { name: 'Элемент 1.2' }
+            ]
+          },
+          { name: 'Элемент 10' },
+          { name: 'Элемент 1' }
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/components/Menu/Item.vue
@@ -0,0 +1,37 @@
+<template>
+  <li :class="getClassName('listItem')">
+    <template v-if="item.childs">
+      <div :class="getClassName('listItemLink','group')" @click="toggleShowChilds">
+        <div>{{item.name}}</div>
+        <i class="material-icons" v-if="item.showChilds===false">expand_more</i>
+        <i class="material-icons" v-else>expand_less</i>
+      </div>
+      <railway-menuItems
+        :show="item.showChilds"
+        :items="item.childs"></railway-menuItems>
+    </template>
+    <template v-else>
+      <a :class="getClassName('listItemLink')">{{item.name}}</a>
+    </template>
+  </li>
+</template>
+<script>
+  export default {
+    name: 'railway-menuItem',
+    props: ['item','show'],
+    methods: {
+      getClassName (el="",mod='') {
+        return this.$parent.getClassName(el,mod)
+      },
+      toggleShowChilds: function(){
+
+        if ('showChilds' in this.item){
+          this.item.showChilds = !this.item.showChilds
+        }else{
+          this.$set(this.item, 'showChilds', false)
+        }
+      }
+    }
+  }
+</script>
+
